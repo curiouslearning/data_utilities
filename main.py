11eb8de613c95b0eae7f26b12a7c37313f46babd
Jeff Oberlander <jeffoberlander@gmail.com>
2023-11-01 13:54:50 -0700

    renamed primary function to main.py as a requirement of Cloud Functions

---
+++ main.py
@@ -0,0 +1,241 @@
+from google.cloud import bigquery
+from google.cloud.exceptions import NotFound
+from datetime import datetime, date, timedelta
+import requests
+import logging
+import json
+import base64
+from facebook_business.api import FacebookAdsApi
+from facebook_business.adobjects.adaccount import AdAccount
+from facebook_business.adobjects.adaccountuser import AdAccountUser
+from facebook_business.adobjects.adsinsights import AdsInsights
+from facebook_business.adobjects.campaign import Campaign
+from google.cloud import secretmanager
+
+client = secretmanager.SecretManagerServiceClient()
+logger = logging.getLogger()
+attributes = {}
+
+
+schema_exchange_rate = [
+    bigquery.SchemaField("date", "DATE", mode="REQUIRED"),
+    bigquery.SchemaField("currencies", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("rate", "FLOAT", mode="REQUIRED"),
+]
+
+schema_facebook_stat = [
+    bigquery.SchemaField("date", "DATE", mode="REQUIRED"),
+    # bigquery.SchemaField("ad_id", "STRING", mode="REQUIRED"),
+    # bigquery.SchemaField("ad_name", "STRING", mode="REQUIRED"),
+    # bigquery.SchemaField("adset_id", "STRING", mode="REQUIRED"),
+    # bigquery.SchemaField("adset_name", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("campaign_id", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("campaign_name", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("start_date", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("end_date", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("clicks", "INTEGER", mode="REQUIRED"),
+    bigquery.SchemaField("impressions", "INTEGER", mode="REQUIRED"),
+    bigquery.SchemaField("reach", "INTEGER", mode="REQUIRED"),
+    bigquery.SchemaField("cpc", "FLOAT", mode="REQUIRED"),
+    bigquery.SchemaField("spend", "FLOAT", mode="REQUIRED"),
+    bigquery.SchemaField(
+        "conversions",
+        "RECORD",
+        mode="REPEATED",
+        fields=(
+            bigquery.SchemaField("action_type", "STRING"),
+            bigquery.SchemaField("value", "STRING"),
+        ),
+    ),
+    bigquery.SchemaField(
+        "actions",
+        "RECORD",
+        mode="REPEATED",
+        fields=(
+            bigquery.SchemaField("action_type", "STRING"),
+            bigquery.SchemaField("value", "STRING"),
+        ),
+    ),
+]
+
+clustering_fields_facebook = ["campaign_id", "campaign_name"]
+
+
+def get_secret(secret):
+    client = secretmanager.SecretManagerServiceClient()
+    name = "projects/405806232197/secrets/" + secret + "/versions/latest"
+    response = client.access_secret_version(name=name)
+    return response.payload.data.decode("UTF-8")
+
+
+def set_attributes():
+    attributes = {
+        "table_id": get_secret("table_id"),
+        "dataset_id": get_secret("dataset_id"),
+        "fb_access_token": get_secret("fb_access_token"),
+        "fb_account_id": get_secret("fb_account_id"),
+        "fb_app_id": get_secret("fb_app_id"),
+        "fb_app_secret": get_secret("fb_app_secret"),
+        "gcp_project_id": get_secret("gcp_project_id"),
+    }
+    return attributes
+
+
+def exist_dataset_table(
+    client, table_id, dataset_id, project_id, schema, clustering_fields=None
+):
+    try:
+        dataset_ref = "{}.{}".format(project_id, dataset_id)
+        client.get_dataset(dataset_ref)  # Make an API request.
+
+    except NotFound:
+        dataset_ref = "{}.{}".format(project_id, dataset_id)
+        dataset = bigquery.Dataset(dataset_ref)
+        dataset.location = "US"
+        dataset = client.create_dataset(dataset)  # Make an API request.
+        logger.info("Created dataset {}.{}".format(client.project, dataset.dataset_id))
+
+    try:
+        table_ref = "{}.{}.{}".format(project_id, dataset_id, table_id)
+        client.get_table(table_ref)  # Make an API request.
+
+    except NotFound:
+        table_ref = "{}.{}.{}".format(project_id, dataset_id, table_id)
+
+        table = bigquery.Table(table_ref, schema=schema)
+
+        table.time_partitioning = bigquery.TimePartitioning(
+            type_=bigquery.TimePartitioningType.DAY, field="date"
+        )
+
+        if clustering_fields is not None:
+            table.clustering_fields = clustering_fields
+
+        table = client.create_table(table)  # Make an API request.
+        logger.info(
+            "Created table {}.{}.{}".format(
+                table.project, table.dataset_id, table.table_id
+            )
+        )
+    return "ok"
+
+
+def insert_rows_bq(client, table_id, dataset_id, project_id, data):
+    table_ref = "{}.{}.{}".format(project_id, dataset_id, table_id)
+    table = client.get_table(table_ref)
+    resp = client.insert_rows_json(
+        json_rows=data,
+        table=table_ref,
+    )
+    print(resp)
+
+
+# logger.info("Success uploaded to table {}".format(table.table_id))
+
+
+def get_facebook_data(event):
+    attributes = set_attributes()
+
+    bigquery_client = bigquery.Client()
+
+    yesterday = date.today() - timedelta(1)
+
+    try:
+        FacebookAdsApi.init(
+            attributes["fb_app_id"],
+            attributes["fb_app_secret"],
+            attributes["fb_access_token"],
+        )
+
+        account = AdAccount("act_" + str(attributes["fb_account_id"]))
+        insights = account.get_insights(
+            fields=[
+                AdsInsights.Field.account_id,
+                AdsInsights.Field.campaign_id,
+                AdsInsights.Field.campaign_name,
+                AdsInsights.Field.date_start,
+                AdsInsights.Field.date_stop,
+                #   AdsInsights.Field.adset_name,
+                #   AdsInsights.Field.adset_id,
+                #   AdsInsights.Field.ad_name,
+                #   AdsInsights.Field.ad_id,
+                AdsInsights.Field.spend,
+                AdsInsights.Field.impressions,
+                AdsInsights.Field.reach,
+                AdsInsights.Field.cpc,
+                AdsInsights.Field.clicks,
+                AdsInsights.Field.actions,
+                AdsInsights.Field.conversions,
+            ],
+            params={
+                "level": "campaign",
+                "time_range": {
+                    "since": yesterday.strftime("%Y-%m-%d"),
+                    "until": yesterday.strftime("%Y-%m-%d"),
+                },
+                "time_increment": 1,
+            },
+        )
+    except Exception as e:
+        logger.info(e)
+        print(e)
+        raise
+
+    fb_source = []
+
+    for index, item in enumerate(insights):
+        actions = []
+        conversions = []
+
+        if "actions" in item:
+            for i, value in enumerate(item["actions"]):
+                actions.append(
+                    {"action_type": value["action_type"], "value": value["value"]}
+                )
+
+        if "conversions" in item:
+            for i, value in enumerate(item["conversions"]):
+                conversions.append(
+                    {"action_type": value["action_type"], "value": value["value"]}
+                )
+
+        fb_source.append(
+            {
+                "date": item["date_start"],
+                # "ad_id": item["adset_id"],
+                #   "ad_name": item["adset_name"],
+                #  "adset_id": item["adset_id"],
+                #   "adset_name": item["adset_name"],
+                "campaign_id": item["campaign_id"],
+                "campaign_name": item["campaign_name"],
+                "start_date": item["date_start"],
+                "end_date": item["date_stop"],
+                "clicks": item["clicks"],
+                "impressions": item["impressions"],
+                "reach": item["reach"],
+                "cpc": item["cpc"],
+                "spend": item["spend"],
+                "conversions": conversions,
+                "actions": actions,
+            }
+        )
+        print("fb_source = " + str(fb_source))
+        if (
+            exist_dataset_table(
+                bigquery_client,
+                attributes["table_id"],
+                attributes["dataset_id"],
+                attributes["gcp_project_id"],
+                schema_facebook_stat,
+                clustering_fields_facebook,
+            )
+            == "ok"
+        ):
+            insert_rows_bq(
+                bigquery_client,
+                attributes["table_id"],
+                attributes["dataset_id"],
+                attributes["gcp_project_id"],
+                fb_source,
+            )
+            return "ok"
